ed758a13ed04a02f7c0f05cdc8be6132de3e9567
Gal Koaz <koazgal@gmail.com>
2022-12-06 01:48:33 +0200

    empty test for recover

--- app/src/main/java/com/example/rocketride/RideSearchActivity.java
+++ app/src/main/java/com/example/rocketride/RideSearchActivity.java
@@ -39,80 +39,47 @@ import java.util.List;
 import java.util.Locale;
 
 
-public class RideSearchActivity extends FragmentActivity{
+public class RideSearchActivity extends FragmentActivity {
 
     Button DateButton, TimeButton;
     TextView DateText, TimeText;
     GoogleMap map;
     SupportMapFragment mapFragment;
     SearchView searchView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ride_search);
-        if (!Places.isInitialized()) {
-            Places.initialize(getApplicationContext(), getString(R.string.api_key), Locale.US);
-        }
-
-        DateButton = findViewById(R.id.date_pick_button);
-        TimeButton = findViewById(R.id.time_pick_button);
-        DateText = findViewById(R.id.date_text);
-        TimeText = findViewById(R.id.time_text);
-        //mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map_picker);
-
-        DateButton.setOnClickListener(view -> handdle_date_click());
-        TimeButton.setOnClickListener(view -> handdle_time_click());
-
-        // Initialize the AutocompleteSupportFragment.
-        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
-                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
-
-        // Specify the types of place data to return.
-        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
-
-        // Set up a PlaceSelectionListener to handle the response.
-        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-            @Override
-            public void onPlaceSelected(@NonNull Place place) {
-                // TODO: Get info about the selected place.
-                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
-            }
-            @Override
-            public void onError(@NonNull Status status) {
-                // TODO: Handle the error.
-                Log.i(TAG, "An error occurred: " + status);
-            }
-        });
+        setContentView(R.layout.empty);
+//        if (!Places.isInitialized()) {
+//            Places.initialize(getApplicationContext(), getString(R.string.api_key), Locale.US);
+//        }
+//
+//
+//        //mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map_picker);
+//
+//        // Initialize the AutocompleteSupportFragment.
+//        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+//                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+//
+//        // Specify the types of place data to return.
+//        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+//
+//        // Set up a PlaceSelectionListener to handle the response.
+//        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+//            @Override
+//            public void onPlaceSelected(@NonNull Place place) {
+//                // TODO: Get info about the selected place.
+//                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+//            }
+//
+//            @Override
+//            public void onError(@NonNull Status status) {
+//                // TODO: Handle the error.
+//                Log.i(TAG, "An error occurred: " + status);
+//            }
+//        });
     }
 
-    public void handdle_date_click(){
 
-        Calendar calendar = Calendar.getInstance();
-
-        int YEAR = calendar.get(calendar.YEAR);
-        int MONTH = calendar.get(calendar.MONTH);
-        int DATE = calendar.get(calendar.DATE);
-
-
-        DatePickerDialog datePickerDialog = new DatePickerDialog(this, (datePicker, year, month, date) -> {
-            String  str = date + "." +  month + "." + year;
-            DateText.setText(str);
-        },YEAR,MONTH,DATE);
-
-        datePickerDialog.show();
-    }
-    public void handdle_time_click(){
-        Calendar calendar = Calendar.getInstance();
-
-        int HOUR = calendar.get(calendar.HOUR);
-        int MINUTE = calendar.get(calendar.MINUTE);
-
-        TimePickerDialog timePickerDialog = new TimePickerDialog(this, ((timePicker, hour, minute) -> {
-            String str = hour +":"+minute;
-            TimeText.setText(str);
-        }),HOUR,MINUTE,true);
-
-        timePickerDialog.show();
-
-    }
 }
