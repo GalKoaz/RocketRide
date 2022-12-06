723ee19161e33d873450a342b19d19949769573a
Amir <amirg00@walla.co>
2022-12-06 13:05:45 +0200

    Change RideSearchActivity design and add some functionality to widgets

--- app/src/main/java/com/example/rocketride/RideSearchActivity.java
+++ app/src/main/java/com/example/rocketride/RideSearchActivity.java
@@ -3,17 +3,21 @@ package com.example.rocketride;
 import static com.facebook.login.widget.ProfilePictureView.TAG;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
@@ -25,26 +29,29 @@ import java.util.Locale;
 
 public class RideSearchActivity extends AppCompatActivity {
 
-    Button DateButton, TimeButton;
-    TextView DateText, TimeText;
-    GoogleMap map;
-    SupportMapFragment mapFragment;
-    SearchView searchView;
+    private String selectedSourcePlace, selectedDestPlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ride_search);
+
+        // Hide action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        // Initialize places
         if (!Places.isInitialized()) {
             Places.initialize(getApplicationContext(), getString(R.string.api_key), Locale.US);
         }
 
+        Button searchButton = findViewById(R.id.searchButton);
 
         //mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map_picker);
 
         // Initialize the AutocompleteSupportFragment.
         AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
-                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment2);
 
         // Specify the types of place data to return.
         autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
@@ -55,6 +62,7 @@ public class RideSearchActivity extends AppCompatActivity {
             public void onPlaceSelected(@NonNull Place place) {
                 // TODO: Get info about the selected place.
                 Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                selectedSourcePlace = place.getName();
             }
 
             @Override
@@ -62,6 +70,45 @@ public class RideSearchActivity extends AppCompatActivity {
                 // TODO: Handle the error.
                 Log.i(TAG, "An error occurred: " + status);
             }
+        });
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment2 = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment2.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment2.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+                Toast.makeText(RideSearchActivity.this, "Place: " + place.getName() + ", " + place.getId(),
+                        Toast.LENGTH_SHORT).show();
+                selectedDestPlace = place.getName();
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+        // Set default string in auto complete fragments
+        autocompleteFragment.setHint("Search for source...");
+        autocompleteFragment2.setHint("Search for destination...");
+
+        // Drivers search button action listener
+        searchButton.setOnClickListener(l -> {
+            Toast.makeText(RideSearchActivity.this, "search button clicked",
+                    Toast.LENGTH_SHORT).show();
+
+            Toast.makeText(RideSearchActivity.this, selectedSourcePlace + ", " + selectedDestPlace,
+                    Toast.LENGTH_SHORT).show();
+
         });
     }
 }
