568a870e3f2cf90f6e39bcfcc60fc7f1ce656cdf
Gal Koaz <koazgal@gmail.com>
2022-12-16 01:45:38 +0200

    Upload ActiveDrives.java, activity_active_drives.xml, recycler_active_drive_row.xml, ActiveDrivesActivityViewAdapter.java

---
+++ app/src/main/java/com/example/rocketride/ActiveDrivesActivityViewAdapter.java
@@ -0,0 +1,69 @@
+package com.example.rocketride;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import java.util.ArrayList;
+
+public class ActiveDrivesActivityViewAdapter extends RecyclerView.Adapter<ActiveDrivesActivityViewAdapter.MyViewHolder> {
+    private Context context;
+    private ArrayList<RideModel> ActDrive;
+    private SelectDriverListener listener;
+
+    public ActiveDrivesActivityViewAdapter(Context context, ArrayList<RideModel> ActDrive, SelectDriverListener listener) {
+        this.context = context;
+        this.ActDrive = ActDrive;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ActiveDrivesActivityViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_active_drive_row, parent, false);
+        return new ActiveDrivesActivityViewAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ActiveDrivesActivityViewAdapter.MyViewHolder holder, int position) {
+        RideModel currDriverRide = ActDrive.get(position);
+        holder.source.setText(currDriverRide.getSource());
+        holder.destination.setText(currDriverRide.getDestination());
+        holder.date.setText(currDriverRide.getDate());
+        holder.pickup.setText(currDriverRide.getPickup());
+//        holder.arrow.setOnClickListener(l -> {
+//            listener.onItemClicked(ActDrive.get(position));
+//        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return ActDrive.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+        ConstraintLayout ActiveDrivesLayout;
+        TextView  source, destination, date, pickup;
+        ImageView arrow;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ActiveDrivesLayout = itemView.findViewById(R.id.ActiveDrivesLayout);
+            source = itemView.findViewById(R.id.srcActiveDriveTextView);
+            destination = itemView.findViewById(R.id.destActiveDriveTextView);
+            date = itemView.findViewById(R.id.activeDriveDateTimeTextView);
+            pickup = itemView.findViewById(R.id.picupActiveDriveTextView);
+            arrow = itemView.findViewById(R.id.arrow_active_view);
+        }
+    }
+}
--- app/src/main/java/com/example/rocketride/MenuActivities/ActiveDrives.java
+++ app/src/main/java/com/example/rocketride/MenuActivities/ActiveDrives.java
@@ -1,25 +1,59 @@
 package com.example.rocketride.MenuActivities;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.rocketride.ActiveDrivesActivityViewAdapter;
+import com.example.rocketride.DriverRideModel;
 import com.example.rocketride.MapsDriverActivity;
 import com.example.rocketride.R;
+import com.example.rocketride.RideModel;
+import com.example.rocketride.SelectDriverListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-public class ActiveDrives extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class ActiveDrives extends AppCompatActivity implements SelectDriverListener {
+    private ArrayList<RideModel> ActDrives = new ArrayList<>();
+    private FirebaseFirestore db;
+    private RecyclerView recyclerView;
+    private String selectedSourcePlace="", selectedDestPlace="", UID;
+    private ActiveDrivesActivityViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_active_drives);
+        db = FirebaseFirestore.getInstance();
+
+        UID = Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid();
 
         // Remove action bar
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        // Recycle view
+        recyclerView = findViewById(R.id.ActDriveRecyclerView);
+
+        // Setup drives history list
+        setupUserActDrives();
 
         // Back arrow button
         ImageView backArrow = findViewById(R.id.leftArrowActiveDrives);
@@ -29,5 +63,68 @@ public class ActiveDrives extends AppCompatActivity {
             Intent switchActivityBecomeDriverIntent = new Intent(this, MapsDriverActivity.class);
             startActivity(switchActivityBecomeDriverIntent);
         });
+    }
+
+    protected void setupUserActDrives(){
+        // TODO: here extract closest rides to current user.
+        //       build all related objects afterwards and push them to
+        //       the associated array list called - "closeRides".
+        if(ActDrives.isEmpty()){
+            Toast.makeText(this, "input is empty.", Toast.LENGTH_SHORT).show();
+            // TODO: just for testing - remove it when not needed....
+            for (int i = 0; i < 20; i++) {
+                ActDrives.add(new RideModel("Ariel", "Tel-Aviv", "16/12/2022 0:19", "Ariel-University"));
+            }
+            adapter = new ActiveDrivesActivityViewAdapter(this, ActDrives, this);
+            recyclerView.setAdapter(adapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        }
+        else{
+            //getAliveRides();
+        }
+    }
+
+
+    public ArrayList<RideModel> getActDrives(){
+        ArrayList<RideModel> expiredRides = new ArrayList<>();
+
+        CollectionReference collectionReference = db.collection("drives");
+        Query query = collectionReference.whereEqualTo("alive", true);
+        query.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                for (QueryDocumentSnapshot document : task.getResult()) {
+
+                    // Init seats array
+                    String[] seatsArr = {
+                            document.getString("near_driver_seat"),
+                            document.getString("left_bottom_seat"),
+                            document.getString("center_bottom_seat"),
+                            document.getString("right_bottom_seat")
+                    };
+
+                    // If user is located in one of the drive seats then update list accordingly
+                    boolean isUserInSeats = Arrays.asList(seatsArr).contains(UID);
+                    if (isUserInSeats){
+                        expiredRides.add(new RideModel(
+                                document.getString("source"),
+                                document.getString("destination"),
+                                document.getString("date"),
+                                document.getString("pickup_name")
+                        ));
+                    }
+
+                    Log.d(TAG, document.getId() + " => " + document.getData());
+                }
+            } else {
+                Log.d(TAG, "Error getting documents: ", task.getException());
+            }
+        });
+        return expiredRides;
+    }
+
+    @Override
+    public void onItemClicked(DriverRideModel driverRideModel) {
+
     }
 }
