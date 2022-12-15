9b76f873fae139637f9e5a8c1b7fba2021cffc9a
Gal Koaz <koazgal@gmail.com>
2022-12-15 23:16:42 +0200

    Upload RideModel.java

---
+++ app/src/main/java/com/example/rocketride/RideModel.java
@@ -0,0 +1,49 @@
+package com.example.rocketride;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.HashMap;
+
+public class RideModel {
+    private final String source;
+    private final String destination;
+    private final String date;
+    private final String pickup;
+    public double price;
+    public Double rating_numerical;
+
+    // Constructor
+    public RideModel(String source, String destination, String date, String pickup) {
+        this.source = source;
+        this.destination = destination;
+        this.date = date;
+        this.pickup = pickup;
+    }
+
+    // getters
+    public String getSource() {
+        return source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getPickup() {
+        return pickup;
+    }
+
+    // hashmap
+    public HashMap<String, String> getHashRideDetails(){
+        HashMap<String, String> RideDetails = new HashMap<>();
+        RideDetails.put("source", this.source);
+        RideDetails.put("destination", this.destination);
+        RideDetails.put("date", this.date);
+        RideDetails.put("pickup", this.pickup);
+        return RideDetails;
+    }
+}
