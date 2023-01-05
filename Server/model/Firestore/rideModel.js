94b7c4a2ad303c12b3cefb9f70999ca2fe521a48
Amir <amirg00@walla.com>
2023-01-05 22:31:16 +0200

    Expand models with some queries needed.

---
+++ Server/model/Firestore/rideModel.js
@@ -0,0 +1,88 @@
+// Firestore db instance
+const connectDB = require('./connect')
+const collectionName = 'drives';
+
+// Database reference
+let db;
+
+const start = async () => {
+    db = await connectDB();
+}
+// start connecting to firestore
+start();
+
+// Get the ride JSON given a ride ID (docmuent's id)
+const getRideByRideID = async (rideID) => {
+    try{
+        const drivesRef = db.collection(collectionName);
+        const queryRef = ratesRef.where('_id', '==', rideID);
+        const querySnapshot = await queryRef.get();
+
+        // Check if there are any results for the query
+        if (!querySnapshot.empty) {
+            const doc = querySnapshot.docs[0];
+            return doc.data();
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error('Error getting document', error);
+    }
+}
+
+// Gets a JSON of attributes to change with their new value, given a ride id.
+const updateRideAttr = async (rideID, attributesJSON) => {
+    try {
+        // Add a new document with a generated ID
+        const docRef = await db.collection(collectionName).doc(rideID).update(attributesJSON);
+        console.log(`DocumentSnapshot updated with ID: ${docRef.id}`);
+    } catch (error) {
+        console.error('Error updating document', error);
+    }
+}
+
+// Adds a ride document given a JSON of ride attributes.
+const addRide = async (rideModel) => {
+    try {
+        // Add a new document with a generated ID
+        const docRef = await db.collection(collectionName).add(rideModel);
+        console.log(`DocumentSnapshot added with ID: ${docRef.id}`);
+    } catch (error) {
+        console.error('Error adding document', error);
+    }
+}
+
+// Get all the alive rides
+const getAliveRides = async () => {
+    const drivesRef = db.collection(collectionName);
+    const queryRef = ratesRef.where('alive', '==', true);
+    try {
+        const querySnapshots = await queryRef.get();
+        return querySnapshots;
+    } catch (error) {
+        console.error('Error getting document', error);
+    }
+}
+
+// Get alive rides documents given a date (as a JSON).
+const getAliveRidesInDate = async (dateJSON) => {
+    const drivesRef = db.collection(collectionName);
+    const queryRef = ratesRef.where('alive', '==', true)
+        .where('date-d', '==', dateJSON.day)
+        .where('date-d', '==', dateJSON.month)
+        .where('date-d', '==', dateJSON.year);
+    try {
+        const querySnapshots = await queryRef.get();
+        return querySnapshots;
+    } catch (error) {
+        console.error('Error getting document', error);
+    }
+}
+
+module.exports = {
+    getRideByRideID,
+    updateRideAttr,
+    addRide,
+    getAliveRides,
+    getAliveRidesInDate
+};
---
+++ Server/model/Firestore/userModel.js
@@ -0,0 +1,35 @@
+// Firestore db instance
+const connectDB = require('./connect')
+const collectionName = 'users';
+
+// Database reference
+let db;
+
+const start = async () => {
+    db = await connectDB();
+}
+// start connecting to firestore
+start();
+
+// Get the user details by his user id
+const getUserDetails = async (userID) => {
+    try{
+        const ratesRef = db.collection(collectionName);
+        const queryRef = ratesRef.where('UID', '==', userID);
+        const querySnapshot = await queryRef.get();
+
+        // Check if there are any results for the query
+        if (!querySnapshot.empty) {
+            const doc = querySnapshot.docs[0];
+            return doc.data();
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error('Error getting document', error);
+    }
+}
+
+module.exports = {
+    getUserDetails
+};
